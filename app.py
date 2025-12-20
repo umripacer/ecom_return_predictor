29d21641005ee4054e4a43b14f529ba46922dc3a
umripacer <225165@aack.au.edu.pk>
2025-12-20 19:46:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -125,8 +125,8 @@ MODEL_METRICS = {
 # -----------------------------
 # Prediction Section
 # -----------------------------
-st.markdown("<div class='glass-card'> 📦 Enter Product & Order Details </div>", unsafe_allow_html=True)
-st.header("📦 Enter Product & Order Details")
+st.markdown("<div class='glass-card'> st.header("📦 Enter Product & Order Details") </div>", unsafe_allow_html=True)
+
 
 col1, col2 = st.columns(2)
 with col1:
@@ -341,5 +341,6 @@ st.markdown("""
         </p>
     </div>
 """, unsafe_allow_html=True)
+
 
 
